9a0f3c481a7d0092d3960f60fd1a55a5610ec968
[bedir] <bedirhan.ali.akcay@gmail.com>
2023-05-15 01:08:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from matplotlib import pyplot as plt
 
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
@@ -18,3 +19,19 @@ df_copy.describe().T
 
 df_copy.nunique()
 df_copy.dropna(inplace=True)
+
+
+df_copy["total_shopping"] = df_copy["order_num_total_ever_online"] + df_copy["order_num_total_ever_offline"]
+df_copy["total_price"] = df_copy["customer_value_total_ever_online"] + df_copy["customer_value_total_ever_offline"]
+
+df_copy["first_order_date"] = pd.to_datetime(df_copy["first_order_date"], format='%Y-%m-%d')
+df_copy["last_order_date"] = pd.to_datetime(df_copy["last_order_date"], format='%Y-%m-%d')
+df_copy["last_order_date_online"] = pd.to_datetime(df_copy["last_order_date_online"], format='%Y-%m-%d')
+df_copy["last_order_date_offline"] = pd.to_datetime(df_copy["last_order_date_offline"], format='%Y-%m-%d')
+
+
+plt.hist(df_copy["last_order_channel"])
+
+plt.xlabel("toplam alışver")
+plt.ylabel("toplam tutar")
+plt.scatter(df_copy["total_shopping"], df_copy["total_price"])
